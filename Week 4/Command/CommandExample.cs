8438cb7a59679546a764150a6d687d4772ca7759
Hamie <h.nouri@sae.edu>
2023-02-10 11:44:07 +0000

    Command Example

---
+++ Week 4/Command/CommandExample.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandExample : MonoBehaviour
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    public class MoveLeftCommand : ICommand
+    {
+        private Character _character;
+
+        public MoveLeftCommand(Character character)
+        {
+            _character = character;
+        }
+
+        public void Execute()
+        {
+            _character.MoveLeft();
+        }
+    }
+
+    public class MoveRightCommand : ICommand
+    {
+        private Character _character;
+
+        public MoveRightCommand(Character character)
+        {
+            _character = character;
+        }
+
+        public void Execute()
+        {
+            _character.MoveRight();
+        }
+    }
+
+    public class Character
+    {
+        public void MoveLeft()
+        {
+        // code to move the character left
+        }
+
+        public void MoveRight()
+        {
+        // code to move the character right
+        }   
+
+        public void ExecuteCommand(ICommand command)
+        {
+            command.Execute();
+        }
+    }
+}
